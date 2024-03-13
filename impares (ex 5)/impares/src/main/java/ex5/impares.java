55e06e93e0abb354b910d806165ce94ff0ccc96e
Kauã Castro <162861675+KauaaCastro@users.noreply.github.com>
2024-03-13 12:41:39 -0300

    Add files via upload

---
+++ impares (ex 5)/impares/src/main/java/ex5/impares.java
@@ -0,0 +1,36 @@
+package ex5;
+
+import java.util.Scanner;
+
+public class impares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+       while(true) {
+        System.out.println("digite o valor de X (limitando-se somente aos números de 1 a 100)");
+      int Nfinal = scanner.nextInt();
+
+      if(Nfinal >= 1 && Nfinal <= 100){
+        System.out.println("Aqui estao alguns números ímpares de 1 a " + Nfinal + ":");
+
+        int impar = 1;
+        while(impar <= Nfinal){
+          if(impar % 2 == 1){
+            System.out.println(impar);
+          }
+          impar++;
+
+        }
+      }
+
+      
+      
+      
+      
+      
+      
+      }        
+    }
+  }
+
